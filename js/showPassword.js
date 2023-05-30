e1bfe0bb3821d003835c40c8ca9aca5a052bb1bc
Gisele Araujo Silva <gisele.araujosilva-ext@ab-inbev.com>
2023-05-30 18:28:45 -0300

    :recycle: Change the name in files

--- js/showPassword.js
+++ js/showPassword.js
@@ -1,5 +1,3 @@
-// exibir/ocultar senha
-
 function togglePasswordVisibility() {
   const passwordInput = document.getElementById("password");
   const showButton = document.getElementById("show_eye");
@@ -11,29 +9,19 @@ function togglePasswordVisibility() {
   hideButton.classList.toggle("d-none");
 }
 
-function login(event) {
-  // Prevenir o envio do formulário por padrão
+function login(event) {  
   event.preventDefault();
 
-  // Obter os valores dos inputs de e-mail e senha
   var email = document.getElementById("username").value;
   var senha = document.getElementById("password").value;
 
-  // Verificar se o e-mail e senha são válidos
   if (email === "teste@teste.com" && senha === "teste") {
-    // Login bem-sucedido
-    console.log("Login realizado com sucesso!");
-
-    // Redirecionar para a página "home.html" após o login
+    console.log("Login realizado com sucesso!");   
     window.location.href = "home.html";
-  } else {
-    // Login inválido
+  } else {    
     alert("Email e senha incorretos!");
-    console.log("E-mail ou senha incorretos. Por favor, tente novamente.");
-
-       // Limpar os campos de e-mail e senha
+    console.log("E-mail ou senha incorretos. Por favor, tente novamente.");      
     document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
-    // Aqui você pode exibir uma mensagem de erro para o usuário
+    document.getElementById("password").value = "";   
   }
 }
--- js/themeToogle.js
+++ js/themeToogle.js
@@ -1,13 +1,10 @@
-// Obtém o elemento checkbox
 const checkbox = document.getElementById("chk");
 
-// Verifica o estado do checkbox quando a página é carregada
 if (localStorage.getItem("theme") === "dark") {
   enableDarkMode();
   checkbox.checked = true;
 }
 
-// Alterna o tema quando o checkbox é alterado
 checkbox.addEventListener("change", function () {
   if (this.checked) {
     enableDarkMode();
@@ -18,12 +15,10 @@ checkbox.addEventListener("change", function () {
   }
 });
 
-// Função para habilitar o modo escuro
 function enableDarkMode() {
   document.documentElement.classList.add("dark");
 }
 
-// Função para desabilitar o modo escuro
 function disableDarkMode() {
   document.documentElement.classList.remove("dark");
 }
